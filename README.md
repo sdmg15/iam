d136376346789d53d30b8e985d8c7efd61d80585
Sonkeng Maldini <sonkengmaldini@gmail.com>
2016-08-31 05:28:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+** IamBot** Version dof_init.1 
+----------
+
+authored by *sdmg15*
+
+*Intentions*
+	This command helps new members or former members of a team to present themselves to others.
+
+	I designed this command because in my team i seen that there were to much recurrence. It means that when a new user
+
+	was comming, we all rushed to ask him his, name,his jobs, which techs does he use.... Only think if the team has more than tenor hundred members ? This will become too hard.
+
+*Usage*
+	The usage of the command is very straightfoward.
+	The user just has to type 
+
+	```/iam create [presentation_text] ``` to present himself to others.
+	Or maybe there was an mistake during creating the presentation? Don't be worried just use
+	```/iam edit [new_introduction]```` and when you have finished type the button ```Enter``` on your keyboard.
+	In order to view the presentation of a specific user you just have to type ```/iam show [user] ``.
---
+++ web/index.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ 
+
+ */
+function load($classname){
+  require $classname.'.php';
+}
+
+spl_autoload_register('load');
+
+
+if(isset($_GET['token'])  && $_GET['token'] == "TOKEN_FROM_YOUR_TEAM")
+{
+     $connect = new UserDao;
+
+     $receivedText = $_GET['text'];
+     $explodedText = explode(' ',$receivedText, 2);
+
+     if($explodedText[0] == 'edit' || $explodedText[0]== 'create'){
+
+      $message = $connect->doIam(htmlspecialchars($_GET['user_name']), $explodedText[1]);
+
+      echo $message;
+    }
+
+
+    else if($explodedText[0] == 'show'){
+      $username = str_replace('@','',trim($explodedText[1]));
+      $message = $connect->getOneUserPresentation($username);
+      echo $message;
+    }
+
+  /* We display informations about the command */
+     else{
+
+        echo '*IamBot Version dof_init.1* by sdmg15'.PHP_EOL;
+        echo 'This command helps you to see introduction of other members. To do that, type `/iam show @username` and then press Enter.'.PHP_EOL;
+        echo 'You can also create your own introduction by typing `/iam create [introduction_text]` and then hit enter if you leave it empty  it will show this guide.'.PHP_EOL;
+        echo 'May be while creating your introduction you made a mistake? Ok just type `/iam edit [new_introduction]`.'.PHP_EOL;
+  
+        echo 'You can view introduction of following members : ``` ';
+        foreach ($connect->getAll() as $value) {  
+          echo $value['user'].'```'.PHP_EOL;
+        }
+        
+        echo 'View the source on <https://gitlab.com/sdmg15/iAm| GitLab> or <https://github.com/sdmg15/iam| on GitHub>.'.PHP_EOL; 
+     }
+
+
+}else {
+
+  echo "*Error: * : sorry the request doesn't come from a verify slack team.";
+}
