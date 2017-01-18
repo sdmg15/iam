41878ed003d45fc929c6e4ff4d818e6f670b07a4
José Cage <josejanuario7@gmail.com>
2017-01-19 00:18:38 +0100

    Move web/ files to src/ and adding PHPUnit integration

--- src/index.php
+++ src/index.php
@@ -1,38 +1,31 @@
 <?php
-/*
- 
 
- */
-function load($classname){
-  require $classname.'.php';
-}
-
-spl_autoload_register('load');
+use IamBot\Config;
+use IamBot\Connection;
 
 
-if(isset($_GET['token'])  && $_GET['token'] == "TOKEN_FROM_YOUR_TEAM")
-{
+if(isset($_GET['token'])  && $_GET['token'] == "TOKEN_FROM_YOUR_TEAM") {
      $connect = new UserDao;
 
      $receivedText = $_GET['text'];
-     $explodedText = explode(' ',$receivedText, 2);
+     $explodedText = explode(' ', $receivedText, 2);
 
-     if($explodedText[0] == 'edit' || $explodedText[0]== 'create'){
+    if($explodedText[0] == 'edit' || $explodedText[0]== 'create') {
 
-      $message = $connect->doIam(htmlspecialchars($_GET['user_name']), $explodedText[1]);
+        $message = $connect->doIam(htmlspecialchars($_GET['user_name']), $explodedText[1]);
 
-      echo $message;
+        echo $message;
     }
 
 
-    else if($explodedText[0] == 'show'){
-      $username = str_replace('@','',trim($explodedText[1]));
-      $message = $connect->getOneUserPresentation($username);
-      echo $message;
+    else if($explodedText[0] == 'show') {
+        $username = str_replace('@', '', trim($explodedText[1]));
+        $message = $connect->getOneUserPresentation($username);
+        echo $message;
     }
 
-  /* We display informations about the command */
-     else{
+    /* We display informations about the command */
+    else{
 
         echo '*IamBot Version dof_init.1* by sdmg15'.PHP_EOL;
         echo 'This command helps you to see introduction of other members. To do so, type `/iam show @username` and then press Enter.'.PHP_EOL;
@@ -41,14 +34,14 @@ if(isset($_GET['token'])  && $_GET['token'] == "TOKEN_FROM_YOUR_TEAM")
   
         echo 'You can view introduction of following members : ``` ';
         foreach ($connect->getAll() as $value) {  
-          echo $value['user'].'```'.PHP_EOL;
+            echo $value['user'].'```'.PHP_EOL;
         }
         
         echo 'View the source on <https://gitlab.com/sdmg15/iAm| GitLab> or <https://github.com/sdmg15/iam| on GitHub>.'.PHP_EOL; 
-     }
+    }
 
 
 }else {
 
-  echo "*Error: * : sorry the request doesn't come from a verify slack team.";
+    echo "*Error: * : sorry the request doesn't come from a verify slack team.";
 }
---
+++ tests/PresentationsTest.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace IamBot\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class PresentationsTest extends TestCase
+{
+  
+    public function testGetAllPresentations()
+    {
+        //
+    }
+  
+    public function testCreatePresentation()
+    {
+        //
+    }
+  
+    public function testUpdatePresentation()
+    {
+    }
+}
